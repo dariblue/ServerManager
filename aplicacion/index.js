d9673ee795f47ed962d6e92de6abd5b47e3e4411
DarioR-Kali <darrodpaj@gmail.com>
2024-06-28 17:26:10 +0200

    Aplicacion inicial desplegada

---
+++ aplicacion/index.js
@@ -0,0 +1,14 @@
+// backend/index.js
+const express = require('express');
+const app = express();
+const port = 3001;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello from the backend!');
+});
+
+app.listen(port, () => {
+    console.log(`Backend server running at http://localhost:${port}`);
+});
---
+++ web/src/pages/Home.js
@@ -0,0 +1,12 @@
+// web/src/pages/Home.js
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Welcome to the Server Management App</h1>
+        </div>
+    );
+};
+
+export default Home;
